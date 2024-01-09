23a2fdbd231dd073ba5ba4820a08ed2dc15c2ae0
Utsavch189 <utsavpokemon9000chatterjee@gmail.com>
2024-01-09 22:13:56 +0530

    add paginated api

--- src/customer/service/get_service.py
+++ src/customer/service/get_service.py
@@ -1,14 +1,28 @@
 from rest_framework import status
 from src.customer.serializer.bike_serializer import BikeSerializer
 from src.admins.models import Bike
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
 
 class GetBikeService:
 
     def get(self,query:dict)->tuple:
         try:
+            count=bike=Bike.objects.count()
             if not query:
                 bike=Bike.objects.all()
                 data=BikeSerializer(bike,many=True).data
+            
+            elif query.get('page') and query.get('page-size') and len(query)==2:
+                bike=Bike.objects.all().order_by('bike_model_id')
+                paginator=Paginator(bike,query.get('page-size'))
+                try:
+                    query=paginator.page(query.get('page'))
+                except PageNotAnInteger:
+                    query=paginator.page(1)
+                except EmptyPage:
+                    query=paginator.page(paginator.num_pages)
+                data=BikeSerializer(query,many=True).data
             
             elif query.get('model_id') and len(query)==1:
                 if not Bike.objects.filter(bike_model_id=query.get('model_id')).exists():
@@ -27,8 +41,9 @@ class GetBikeService:
                 raise Exception("you pass params in wrong way. You must pass either /get-bikes/ or /get-bikes?model_id=<model_id>/ or /get-bikes?model_name=<model_name>/")
 
             return (
-                    {"bikes":data},
+                    {"bikes":data,"total_bikes":count},
                     status.HTTP_200_OK
                 )
         except Exception as e:
             raise Exception(str(e))
+    
